70374485111d31e7217d5c573731ac0c707686ab
bgreer <greergit@proton.me>
2024-07-25 14:24:25 -0500

    Created grid resolution, main etch-a-sketch functionality working

--- etchasketch.js
+++ etchasketch.js
@@ -1,13 +1,36 @@
 let container = document.querySelector('div[class=container]');
 
+cntWidth = 1000;
+cntWidthStr = String(`${cntWidth}px`);
+container.style.width = cntWidthStr;
 
 
+let resolution = 24;
+let numSquares = resolution*resolution;
+let sqWidth = cntWidth / resolution;
+let sqWidthStr = String(sqWidth);
+console.log("sqWidth: ",sqWidth);
+console.log("sqWidthStr: ", sqWidthStr);
 
-let numSquares = 16;
+container.addEventListener('mouseover', (e) => {
+    target = e.target;
+    target.style.backgroundColor = "red";
+})
+
+container.addEventListener('mouseup', (e) => {
+    if (e.button == 1) {
+        sqDivs = container.querySelectorAll('div');
+        for (div of sqDivs) {
+            div.style.backgroundColor = "black";
+        }
+    }
+})
 
 for (let i = numSquares; i > 0; i--) {
     let sqDiv = document.createElement('div');
-    sqDiv.style.cssText = ("width: 200px; height: 200px; color: white; background-color:black");
+    console.log("SqDiv height ",sqDiv.style.height);
+    sqDiv.style.cssText = ("color: white; background-color:black");
+    sqDiv.style.width = `${sqWidthStr}px`;
+    sqDiv.style.height = `${sqWidthStr}px`;
     container.appendChild(sqDiv);
-    sqDiv.textContent = `${i}`;
 }
